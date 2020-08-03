390efdc96cd975b5a74e9c18a8578a1d1b644f54
Kaili Shan <kailishan@icloud.com>
2020-08-03 08:56:02 -0700

    SonarCloud set up

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
+ 
 import javax.swing.*;
 import java.awt.*;
 
